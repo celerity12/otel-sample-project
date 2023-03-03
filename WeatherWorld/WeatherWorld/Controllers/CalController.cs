8572929a977f5cbb376e9b5c7f157b51d25f63d9
celerity12 <celerity12@gmail.com>
2023-03-03 13:26:03 -0800

    Weather World Initial Commit

---
+++ WeatherWorld/WeatherWorld/Controllers/CalController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using WeatherWorld.Models;
+using Serilog;
+
+namespace WeatherWorld.Controllers
+{
+    public class CalController : Controller
+    {
+        private readonly ILogger<CalController> _logger;
+
+        public CalController(ILogger<CalController> logger)
+        {
+            Log.Logger = new LoggerConfiguration().WriteTo.File("calculator-log-.txt", rollingInterval: RollingInterval.Month).CreateLogger();
+            _logger = logger;
+        }
+
+        // GET: /<controller>/
+        public IActionResult Cal()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add()
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(HttpContext.Request.Form["Text1"].ToString());
+                int num2 = Convert.ToInt32(HttpContext.Request.Form["Text2"].ToString());
+                ViewBag.Result = (num1 + num2).ToString();
+                Log.Information((num1 + num2).ToString());
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Result = "Wrong Input Provided.";
+                Log.Error(ex.StackTrace + ex.Message);
+            }
+            return View("Cal");
+        }
+
+        [HttpPost]
+        public IActionResult Minus()
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(HttpContext.Request.Form["Text1"].ToString());
+                int num2 = Convert.ToInt32(HttpContext.Request.Form["Text2"].ToString());
+                ViewBag.Result = (num1 - num2).ToString();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Result = "Wrong Input Provided.";
+                Log.Error(ex.StackTrace + ex.Message);
+            }
+            return View("Cal");
+        }
+
+        [HttpPost]
+        public IActionResult Multiply()
+        {
+            try
+            {
+                int num1 = Convert.ToInt32(HttpContext.Request.Form["Text1"].ToString());
+                int num2 = Convert.ToInt32(HttpContext.Request.Form["Text2"].ToString());
+                ViewBag.Result = (num1 * num2).ToString();
+                Log.Information((num1 * num2).ToString());
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Result = "Wrong Input Provided.";
+                Log.Error(ex.StackTrace + ex.Message);
+            }
+            return View("Cal");
+        }
+
+        [HttpPost]
+        public IActionResult Divide()
+        {
+            try
+            {
+                decimal num1 = Convert.ToDecimal(HttpContext.Request.Form["Text1"].ToString());
+                decimal num2 = Convert.ToDecimal(HttpContext.Request.Form["Text2"].ToString());
+                decimal f = num1 / num2;
+                ViewBag.Result = f.ToString();
+                Log.Information(f.ToString());
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Result = "Wrong Input Provided.";
+                Log.Error(ex.StackTrace + ex.Message);
+            }
+            return View("Cal");
+        }
+    }
+}
---
+++ WeatherWorld/WeatherWorld/Controllers/HomeController.cs
@@ -0,0 +1,74 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Reflection.Emit;
+using Microsoft.AspNetCore.Mvc;
+using WeatherWorld.Models;
+using Serilog;
+
+namespace WeatherWorld.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        Log.Logger = new LoggerConfiguration().WriteTo.File("weatherlog-.txt", rollingInterval: RollingInterval.Year).CreateLogger();
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    [HttpPost]
+    public IActionResult WeatherAPI()
+    {
+        try
+        {
+            var rng = new Random();
+            Thread.Sleep(2000);
+
+            var TemperatureC = rng.Next(-200, 200);
+            if (TemperatureC > 0)
+            {
+                var client = new HttpClient();
+                client.DefaultRequestHeaders.Add("User-Agent", "C# console program");
+
+                string url = "https://api.weather.gov/points/34.0522,-118.2437";
+                string content = client.GetStringAsync(url).Result;
+                ViewBag.Result = content;
+                Log.Information("Successful ...  " + content);
+            }
+            else if (TemperatureC == 0)
+            {
+                ViewBag.Result = "Please try again ...";
+                _logger.LogInformation("TemperatureC is zero Please try again ...");
+                Log.Warning("TemperatureC is zero Please try again ...");
+            }
+            else
+            {
+                int j = 0;
+                int i = j / 0;
+            }
+        }
+        catch (Exception ex)
+        {
+            ViewBag.Result = "Error: " + ex.Message + ex.StackTrace;
+            _logger.LogError(ex, ex.Message);
+            Log.Error(ex.StackTrace + ex.Message);
+        }
+        return View("Index");
+    }
+
+
+}
+
